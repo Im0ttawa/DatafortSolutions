ec370dcf6fa9ca04378f65e158554cda99fdcf47
Alex <cs.dev.alex.barros@proton.me>
2025-01-12 18:38:23 +0000

    Add files via upload

---
+++ Networking tool.ps1
@@ -0,0 +1,10 @@
+﻿$subnet = "192.168.1"
+$range = 1..255
+
+foreach ($i in $range) {
+    $ip = $subnet + "." + $i
+    if (Test-Connection -ComputerName $ip -Count 1 `
+        -Quiet) {
+            Write-Host "$ip is up"
+     }
+   }
